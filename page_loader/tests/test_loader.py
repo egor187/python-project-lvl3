69f003585706fd863e5eea4f4bbc8bb24d8e1d15
Егор <oegor8194@gmail.com>
2021-01-20 11:57:25 +0300

    add test for correct filename of downloaded img

--- page_loader/tests/test_loader.py
+++ page_loader/tests/test_loader.py
@@ -24,5 +24,10 @@ def test_iscorrect_dir_name():
 
 
 def test_iscorrect_img_name():
-    pass
+    with tempfile.TemporaryDirectory() as tmpdir:
+        url = "https://page-loader.hexlet.repl.co/"
+        download(url, tmpdir)
+        expect_dir_name = os.path.join(tmpdir, "page-loader-hexlet-repl-co_files")
+        expect_img_file_name = os.path.join(tmpdir, expect_dir_name,  "page-loader-hexlet-repl-co-assets-professions-nodejs.png")
+        assert os.path.isfile(expect_img_file_name)
 
